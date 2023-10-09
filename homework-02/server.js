41f134efe3d4e798a48911ee5eec93b2babc23ec
andriytol <andriyytolstoy@gmail.com>
2023-10-09 10:47:03 +0300

    add app.js

--- homework-02/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app'); // Підключення модуля `app`
-
-app.listen(3000, () => {
-  // Запуск сервера на порті 3000
-  console.log('Server running. Use our API on port: 3000'); // Вивід повідомлення у консолі
-});
---
+++ routes/api/contacts.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const contactsController = require('../../controllers/contacts-controller');
+
+const schemas = require('../../schemas/contacts-schemas');
+
+const { validateBody } = require('../../decorators');
+
+const router = express.Router();
+
+router.get('/', contactsController.getAllContacts);
+
+router.get('/:id', contactsController.getContactById);
+
+router.post('/', validateBody(schemas.contactAddSchema), contactsController.addContact);
+
+router.delete('/:id', contactsController.removeContact);
+
+router.put('/:id', validateBody(schemas.contactAddSchema), contactsController.updateContact);
+
+module.exports = router;
