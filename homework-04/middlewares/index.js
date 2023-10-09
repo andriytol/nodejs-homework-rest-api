a7e62e50f17938f02897ae2fca87e8d47f1e8fea
andriytol <andriyytolstoy@gmail.com>
2023-10-09 10:51:19 +0300

    add app.js

--- homework-04/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-const isValidId = require('./isValidId');
-const authenticate = require('./authenticate');
-const validateBody = require('./validateBody');
-
-module.exports = { isValidId, authenticate, validateBody };
